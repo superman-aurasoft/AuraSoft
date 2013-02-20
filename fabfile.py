42a85420f9a6df89765da6e7201ce5bece78ef32
Myunghun Cha <scorpiox.kr@gmail.com>
2013-02-20 10:27:45 +0900

    asdfasdf

--- fabfile.py
+++ fabfile.py
@@ -14,7 +14,5 @@ def deploy():
     run("cd /var/git/AuraSoft/; git pull");
     sudo("rm -rf /home/aurasoft/www/AuraSoft");
     sudo("cp -R /var/git/AuraSoft/ /home/aurasoft/www/");
-#    sudo("mkdir /home/aurasoft/www/AuraSoft/upload");
-#    sudo("mkdir /home/aurasoft/www/AuraSoft/upload/CACHE");
-#    sudo("mkdir /home/aurasoft/www/AuraSoft/upload/CACHE/js");
+    sudo("mv /home/aurasoft/www/AuraSoft/Aurasoft/settings.deploy.py /home/aurasoft/www/Aurasoft/settings.py");
     sudo("chown -R apache.apache /home/aurasoft/www/");
